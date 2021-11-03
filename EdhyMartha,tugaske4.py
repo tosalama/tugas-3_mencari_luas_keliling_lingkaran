a0f78cca5daef55d4868e32954d7b6552b05b8bf
tosalama <edhymartha1@gmail.com>
2021-11-03 01:40:20 -0700

    menambahkan file tugas pak edi1.py

---
+++ EdhyMartha,tugaske4.py
@@ -0,0 +1,7 @@
+print("program mencari luas dan keliling lingkaran")
+r = int(input("masukan jari-jari = "))
+phi= 22/7
+luas = phi*r*r
+keliling = 2*phi*r
+print("luas lingkaran adalah     = ",luas)
+print("keliling lingkaran adalah =", keliling)
